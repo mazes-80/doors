3819ae48c39b9500b6b19dd5b6b3e7033f525ace
TenPlus1 <kinsellaja@yahoo.com>
2017-03-31 10:08:37 +0100

    removed def.groups for door inventory items, was removing them from creative inv.

--- init.lua
+++ init.lua
@@ -260,7 +260,6 @@ function doors.register(name, def)
 	minetest.register_craftitem(":" .. name, {
 		description = def.description,
 		inventory_image = def.inventory_image,
-		groups = def.groups,
 
 		on_place = function(itemstack, placer, pointed_thing)
 			local pos = nil
