ee9a1b45e1e5a9a959d27e336ec36d08d0f3e6eb
mazes 80 <>
2022-09-26 06:15:50 +0200

    Allow to preserve door secret when digging

--- init.lua
+++ init.lua
@@ -193,6 +193,100 @@ local can_toggle = function(clicker, pos)
 	return false
 end
 
+local function on_dig_door(pos, node, digger)
+	if not digger:get_player_control().sneak then
+		return core.node_dig(pos, node, digger)
+	end
+
+	local diggername = digger:get_player_name()
+	local log = (diggername and core.log) or function() end
+	local def = core.registered_nodes[node.name]
+
+	if ( not can_dig_door(pos, digger) ) then
+		log("action", diggername
+				.. " tried to dig " .. node.name
+				.. " at protected position "
+				.. core.pos_to_string(pos))
+		core.record_protection_violation(pos, diggername)
+		return false
+	end
+
+	log('action', diggername .. " digs "
+		.. node.name .. " at " .. core.pos_to_string(pos))
+
+	local wielded = digger and digger:get_wielded_item()
+	local drops = core.get_node_drops(node, wielded and wielded:get_name())
+
+	if wielded then
+		local wdef = wielded:get_definition()
+		local tp = wielded:get_tool_capabilities()
+		local dp = core.get_dig_params(def and def.groups, tp, wielded:get_wear())
+		if wdef and wdef.after_use then
+			wielded = wdef.after_use(wielded, digger, node, dp) or wielded
+		else
+			-- Wear out tool
+			if not core.is_creative_enabled(diggername) then
+				wielded:add_wear(dp.wear)
+				if wielded:get_count() == 0 and wdef.sound and wdef.sound.breaks then
+					core.sound_play(wdef.sound.breaks, {
+						pos = pos,
+						gain = 0.5
+					}, true)
+				end
+			end
+		end
+		digger:set_wielded_item(wielded)
+	end
+	local secret = core.get_meta(pos):get_string("key_lock_secret")
+	if secret ~= "" then
+		local stack
+		for k, v in pairs(drops) do
+			stack = ItemStack(v)
+			stack:set_meta("key_lock_secret", secret)
+			drops[k] = stack
+		end
+	end
+
+	-- Handle drops
+	core.handle_node_drops(pos, drops, digger)
+
+	local oldmetadata = nil
+	if def and def.after_dig_node then
+		oldmetadata = core.get_meta(pos):to_table()
+	end
+
+	-- Remove node and update
+	core.remove_node(pos)
+
+	-- Play sound if it was done by a player
+	if diggername ~= "" and def and def.sounds and def.sounds.dug then
+		core.sound_play(def.sounds.dug, {
+			pos = pos,
+			exclude_player = diggername,
+		}, true)
+	end
+
+	-- Run callback
+	if def and def.after_dig_node then
+		-- Copy pos and node because callback can modify them
+		local pos_copy = vector.copy(pos)
+		local node_copy = {name=node.name, param1=node.param1, param2=node.param2}
+		def.after_dig_node(pos_copy, node_copy, oldmetadata, digger)
+	end
+
+	-- Run script hook
+	for _, callback in ipairs(core.registered_on_dignodes) do
+		local origin = core.callback_origins[callback]
+		core.set_last_run_mod(origin.mod)
+
+		-- Copy pos and node because callback can modify them
+		local pos_copy = vector.copy(pos)
+		local node_copy = {name=node.name, param1=node.param1, param2=node.param2}
+		callback(pos_copy, node_copy, digger)
+	end
+
+	return true
+end
 
 -- this hidden node is placed on top of the bottom, and prevents
 -- nodes from being placed in the top half of the door.
@@ -609,6 +703,8 @@ function doors.register(name, def)
 		minetest.remove_node({x = pos.x, y = pos.y + 1, z = pos.z})
 	end
 
+	def.on_dig = on_dig_door
+
 	def.drawtype = "mesh"
 	def.paramtype = "light"
 	def.paramtype2 = "facedir"
@@ -837,6 +933,8 @@ function doors.register_trapdoor(name, def)
 	def.is_ground_content = false
 
 	def.can_dig = can_dig_door
+
+	def.on_dig = on_dig_door
 
 	def.after_place_node = function(pos, placer, itemstack, pointed_thing)
 
